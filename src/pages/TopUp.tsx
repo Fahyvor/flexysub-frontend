e3d0e7901e3b1327953c278d3103422f54b44b45
Fahyvor <favourokafor30@gmail.com>
2024-12-17 18:35:46 +0100

    more pushes

--- src/pages/TopUp.tsx
+++ src/pages/TopUp.tsx
@@ -1,49 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { API_URL } from '../axios/apiUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const TopUp: React.FC = () => {
     const [formData, setFormData] = useState({
         network: '',
-        number: '',
+        phone: '',
         service: '',
         plan: '',
         amount: '',
         name: '',
     });
+    const [isLoading, setIsLoading] = useState(false);
 
-    const [dataPlans, setDataPlans] = useState<string[]>([]);
-    const [dataAmount, setDataAmount] = useState<string[]>([]);
-    const [showDataPlansInput, setDataPlansInput] = useState<boolean>(false)
+    // const [dataPlans, setDataPlans] = useState<string[]>([]);
+    // const [dataAmount, setDataAmount] = useState<string[]>([]);
+    // const [showDataPlansInput, setDataPlansInput] = useState<boolean>(false)
 
-    useEffect(() => {
-        const fetchDataPlans = async () => {
-            if (formData.service !== "Data") {
-                setDataPlansInput(false);
-                return;
-            }
+    // useEffect(() => {
+    //     const fetchDataPlans = async () => {
+    //         if (formData.service !== "Data") {
+    //             // setDataPlansInput(false);
+    //             return;
+    //         }
     
-            setDataPlansInput(true);
+    //         // setDataPlansInput(true);
     
-            try {
-                const response = await axios.get(`${API_URL}api/vtu/get-data-plans`);
+    //         try {
+    //             const response = await axios.get(`${API_URL}api/vtu/get-data-plans`);
     
-                // Log the response to confirm the structure
-                console.log("API Response:", response.data);
+    //             // Log the response to confirm the structure
+    //             console.log("API Response:", response.data);
     
-                if (response.status === 200 && Array.isArray(response.data.data)) {
-                    setDataPlans(response.data.data);
-                    setDataAmount(response.data.dataAmount);
-                } else {
-                    console.error("Unexpected data format:", response.data);
-                }
-            } catch (error) {
-                console.error("Error getting data", error);
-            }
-        };
+    //             if (response.status === 200 && Array.isArray(response.data.data)) {
+    //                 setDataPlans(response.data.data);
+    //                 setDataAmount(response.data.dataAmount);
+    //             } else {
+    //                 console.error("Unexpected data format:", response.data);
+    //             }
+    //         } catch (error) {
+    //             console.error("Error getting data", error);
+    //         }
+    //     };
     
-        fetchDataPlans();
-    }, [formData.service])   
+    //     fetchDataPlans();
+    // }, [formData.service])   
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
         const { id, value } = e.target;
@@ -52,15 +55,15 @@ const TopUp: React.FC = () => {
             // Check if the changed field is the plan name
             if (id === "name") {
                 // Find the index of the selected plan
-                const selectedPlanIndex = dataPlans.indexOf(value);
-                // Use the index to find the corresponding amount
-                const correspondingAmount = selectedPlanIndex !== -1 ? dataAmount[selectedPlanIndex] : "";
+                // const selectedPlanIndex = dataPlans.indexOf(value);
+                // // Use the index to find the corresponding amount
+                // const correspondingAmount = selectedPlanIndex !== -1 ? dataAmount[selectedPlanIndex] : "";
     
-                return {
-                    ...prevData,
-                    [id]: value, // Update the plan name
-                    amount: correspondingAmount, // Set the amount based on the plan index
-                };
+                // return {
+                //     ...prevData,
+                //     [id]: value, // Update the plan name
+                //     amount: correspondingAmount, // Set the amount based on the plan index
+                // };
             }
     
             // For other fields, update normally
@@ -74,16 +77,35 @@ const TopUp: React.FC = () => {
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
+        setIsLoading(true);
         try {
-            const response = await axios.post(`${API_URL}/vtu/submit`, formData);
-            console.log('Response:', response.data);
+            if(formData.service === "airtime") {
+                const response = await axios.post(`${API_URL}api/vtu/buy-airtime`, formData);
+                toast.success(response.data.data.response_description);
+                console.log('Response:', response.data);
+            } else {
+                const response = await axios.post(`${API_URL}api/vtu/buy-data`, formData);
+                toast.success(response.data.message);
+                console.log('Response:', response.data);
+            }
         } catch (error) {
-            console.error("Error submitting form:", error);
+            if (axios.isAxiosError(error)) {
+                console.log('Airtime Purchase:', error.message);
+                toast.error(error.response?.data?.error || 'An error occurred');
+            } else {
+                console.log('Unexpected error:', error);
+                toast.error('An unexpected error occurred');
+            }
+        } finally {
+            setIsLoading(false);
         }
     };
 
     return (
         <div className='w-full'>
+             <div className='toastify-message'>
+                <ToastContainer />
+            </div>
             <form onSubmit={handleSubmit} className='flex flex-col gap-9 py-5 w-full'>
                 <div className='flex flex-col gap-3'>
                     <label htmlFor="network" className='text-semibold'>Network Type:</label>
@@ -95,10 +117,10 @@ const TopUp: React.FC = () => {
                         className='p-2 outline-none rounded-lg px-5'
                     >
                         <option value="">Select a network</option>
-                        <option value="1">MTN</option>
-                        <option value="2">Airtel</option>
-                        <option value="3">Glo</option>
-                        <option value="4">9mobile</option>
+                        <option value="mtn">MTN</option>
+                        <option value="airtel">Airtel</option>
+                        <option value="glo">Glo</option>
+                        <option value="etisalat">9mobile</option>
                     </select>
                 </div>
 
@@ -112,13 +134,12 @@ const TopUp: React.FC = () => {
                         className='p-2 outline-none rounded-lg px-5'
                     >
                         <option value="">Select a service</option>
-                        <option value="Data">Data</option>
-                        <option value="Airtime">Airtime</option>
+                        <option value="data">Data</option>
+                        <option value="airtime">Airtime</option>
                     </select>
                 </div>
 
-                {showDataPlansInput ? 
-                   <div className='flex flex-col gap-5'>
+                   {/* <div className='flex flex-col gap-5'>
                         <div className="flex flex-col gap-3">
                             <label htmlFor="name" className="text-semibold">Select Plan Name:</label>
                             <select
@@ -153,8 +174,7 @@ const TopUp: React.FC = () => {
                                 ))}
                             </select>
                         </div>
-                    </div>
-                    :
+                    </div> */}
                     <div className='flex flex-col gap-3'>
                         <label htmlFor="amount" className="text-semibold">Input Amount</label>
                         <input
@@ -165,21 +185,20 @@ const TopUp: React.FC = () => {
                             className="p-2 outline-none rounded-lg bg-gray-200 px-5 w-full"
                         />
                     </div>
-                }
 
                 <div className='flex flex-col gap-3'>
-                    <label htmlFor="number">Number:</label>
+                    <label htmlFor="phone">Number:</label>
                     <input
                         type="tel"
-                        id="number"
-                        value={formData.number}
+                        id="phone"
+                        value={formData.phone}
                         onChange={handleInputChange}
                         required
                         className='bg-gray-200 p-2 outline-none rounded-lg px-5 py-3'
                     />
                 </div>
 
-                <button type="submit" className='bg-gold p-3 rounded-lg lg:w-1/2 md:w-1/2 w-full my-4'>Submit</button>
+                <button type="submit" className='bg-gold p-3 rounded-lg lg:w-1/2 md:w-1/2 w-full my-4'>{isLoading ? "Submitting" : "Submit"}</button>
             </form>
         </div>
     );
